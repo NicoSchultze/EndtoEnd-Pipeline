1067bb427694b5d8491562ffbfd86550fd5ef5d9
NicoSchultze <nico.schultze97@gmail.com>
2024-03-11 13:26:52 +0100

    setup and test api connection

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -10,6 +10,9 @@ import requests
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 import json
+from kafka import KafkaProducer
+import time
+import logging
 
 # Define default arguments for the DAG
 default_args = {
@@ -48,16 +51,29 @@ def format_data(res):
 
 # Function to stream formatted data
 def stream_data():
-    from kafka import KafkaProducer
-    import time
-    res = get_data()
-    res = format_data(res)
-    # print(json.dumps(res, indent=3))
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
 
-    producer = KafkaProducer(bootstrap_servers='localhost:9092', max_block_ms=5000)
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
 
-    # push data from the api to the queue (its called topic?)
-    producer.send('users_created', json.dumps(res).encode('utf-8'))
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
 
 
-stream_data()
+with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
+
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
+
